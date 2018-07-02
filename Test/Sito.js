e372ccbac7cae20643672d6ba690ae73ab33ce01
Zbarley <Simonezorzo@gmail.com>
2018-07-03 00:25:47 +0200

    Update Sito.js

--- Test/Sito.js
+++ Test/Sito.js
@@ -36,33 +36,19 @@ function setup() {
   firebase.initializeApp(config);
   database = firebase.database();
 
-  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-  .then(function() {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
-    return firebase.auth().signInWithEmailAndPassword(email, password){
-          // This gives you a Google Access Token. You can use it to access the Google API.
-      token = result.credential.accessToken;
-      // The signed-in user info.
-      user = result.user;;
-     }}
-  .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE)
+      .then(function() {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // In memory persistence will be applied to the signed in Google user
+        // even though the persistence was set to 'none' and a page redirect
+        // occurred.
+        return firebase.auth().signInWithRedirect(provider);
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
   });
-
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-
-  }).catch(function(error) {
-  console.log(error.code + " " + error.message);
-  });
-  console.log(token);
-  console.log(user);
-
   createCanvas(100, 100).parent("#root");
   rgbDiv = createDiv().parent("#root");
   createCanvas(200, 200).parent('#root');
