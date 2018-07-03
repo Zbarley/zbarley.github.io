b8895fb81db1c740fdcde6e17940c1a87ed68ea9
Zbarley <Simonezorzo@gmail.com>
2018-07-03 14:20:29 +0200

    Create sketch.js

---
+++ Update/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+		  // Initialize Firebase
+		  var config = {
+				apiKey: "AIzaSyASOr3yFQVk4yRcp0uzkKWI1lX0ewYdLtM",
+				authDomain: "color-classifier-zbarley.firebaseapp.com",
+				databaseURL: "https://color-classifier-zbarley.firebaseio.com",
+				projectId: "color-classifier-zbarley",
+				storageBucket: "color-classifier-zbarley.appspot.com",
+				messagingSenderId: "646085122999"
+		  };
+		  firebase.initializeApp(config);
+		  database= firebase.database();
