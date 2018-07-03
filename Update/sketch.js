cbd5c29737bcf62b0bef1b775ce5539ae64984bc
Zbarley <Simonezorzo@gmail.com>
2018-07-03 14:27:34 +0200

    Update sketch.js

--- Update/sketch.js
+++ Update/sketch.js
@@ -1,14 +1,19 @@
 function setup() {
-		// Initialize Firebase
-		var config = {
+		  // Initialize Firebase
+		  var config = {
 				apiKey: "AIzaSyASOr3yFQVk4yRcp0uzkKWI1lX0ewYdLtM",
 				authDomain: "color-classifier-zbarley.firebaseapp.com",
 				databaseURL: "https://color-classifier-zbarley.firebaseio.com",
 				projectId: "color-classifier-zbarley",
 				storageBucket: "color-classifier-zbarley.appspot.com",
 				messagingSenderId: "646085122999"
-		};
-		firebase.initializeApp(config);
-		database= firebase.database();
-		console.log(database);
+		  };
+		  firebase.initializeApp(config);
+		  database= firebase.database();
+
+		  let ref = database.ref('colors')
+		  ref.once('value',gotData;
+}
+function gotData(results){
+	console.log(restults.val());
 }
